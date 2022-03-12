0b2044e23263027e3a131fef4196d06dd20cce80
hugosenari <hugosenari@gmail.com>
2022-03-12 03:28:55 -0300

    chore: re index telenim

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A high-level async TDLib wrapper for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-telenim-master.flake = false;
+  inputs.src-telenim-master.owner = "Yardanico";
+  inputs.src-telenim-master.ref   = "refs/heads/master";
+  inputs.src-telenim-master.repo  = "telenim";
+  inputs.src-telenim-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-telenim-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-telenim-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
